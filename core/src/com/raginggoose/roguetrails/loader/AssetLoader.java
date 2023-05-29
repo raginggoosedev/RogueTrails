183708499663bd9c877d430234ca4b1bd33ef875
ExplorerDonutz <mwquick04@gmail.com>
2023-05-29 19:36:58 -0400

    Added a menu screen with a temp title image and buttons

--- core/src/com/raginggoose/roguetrails/loader/AssetLoader.java
+++ core/src/com/raginggoose/roguetrails/loader/AssetLoader.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.SkinLoader;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class AssetLoader {
     public final AssetManager manager = new AssetManager();
@@ -13,8 +14,14 @@ public class AssetLoader {
     public static final String PLAYER_ATLAS = "playerSprites/player.atlas";
     public static final String PLAYER_TEXTURE = "playerSprites/player.png";
 
+    // Game Skin
+    public static final String GAME_SKIN = "skin.json";
+    public static final String SKIN_ATLAS = "skin.atlas";
+
     public void queueAssets() {
         manager.load(PLAYER_ATLAS, TextureAtlas.class);
         manager.load(PLAYER_TEXTURE, Texture.class);
+        manager.load(SKIN_ATLAS, TextureAtlas.class);
+        manager.load(GAME_SKIN, Skin.class, new SkinLoader.SkinParameter(SKIN_ATLAS));
     }
 }
--- core/src/com/raginggoose/roguetrails/screens/MenuScreen.java
+++ core/src/com/raginggoose/roguetrails/screens/MenuScreen.java
@@ -1,16 +1,57 @@
 package com.raginggoose.roguetrails.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.raginggoose.roguetrails.RogueTrails;
+import com.raginggoose.roguetrails.loader.AssetLoader;
 
 public class MenuScreen implements Screen {
     private final Stage stage;
-    public MenuScreen(RogueTrails game) {
-        stage = new Stage();
-        Table table = new Table();
+    private final Skin skin;
 
+    public MenuScreen(RogueTrails game) {
+        stage = new Stage(new ScreenViewport());
+        skin = game.getAssetManager().manager.get(AssetLoader.GAME_SKIN);
+        Gdx.input.setInputProcessor(stage);
+
+        Table table = new Table();
+        table.setFillParent(true);
+
+        Image image = new Image(skin, "title");
+        table.add(image).padTop(20.0f).padLeft(20.0f).padRight(20.0f).expandX().expandY().fill();
+
+        table.row().expandX().expandY();
+        TextButton textButton = new TextButton("Play", skin);
+        textButton.addListener(new ClickListener() {
+           @Override
+           public void clicked(InputEvent event, float x, float y) {
+               game.setScreen(ScreenType.GAME);
+           }
+        });
+        table.add(textButton).padTop(40.0f).padBottom(10.0f).padLeft(140.0f).padRight(140.0f).expandX().expandY().fill();
+
+        table.row().expandX().expandY();
+        textButton = new TextButton("Settings", skin);
+        table.add(textButton).padTop(10.0f).padBottom(10.0f).padLeft(140.0f).padRight(140.0f).expandX().expandY().fill();
+
+        table.row().expandX().expandY();
+        textButton = new TextButton("Quit", skin);
+        textButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+        table.add(textButton).padTop(10.0f).padBottom(20.0f).padLeft(140.0f).padRight(140.0f).expandX().expandY().fill();
+        stage.addActor(table);
 
     }
 
@@ -21,7 +62,8 @@ public class MenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
