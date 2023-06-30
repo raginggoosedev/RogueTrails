c618138ee016de6bce38c05113c175d6b75dd186
ExplorerDonutz <mwquick04@gmail.com>
2023-06-30 17:40:02 -0400

    Added resize

--- core/src/com/raginggoose/roguetrails/screens/GameScreen.java
+++ core/src/com/raginggoose/roguetrails/screens/GameScreen.java
@@ -170,7 +170,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        cam.setToOrtho(false, width, height);
+        cam.update();
     }
 
     @Override
