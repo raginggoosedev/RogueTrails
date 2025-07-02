7633fa851910e158aad0d8af06e87968aeb506a2
ExplorerDonutz <mwquick04@gmail.com>
2025-07-01 20:52:50 -0400

    Switched main class to extend Game, added getBatch for future screens.

--- core/src/main/java/com/rgd/roguetrails/RogueTrails.java
+++ core/src/main/java/com/rgd/roguetrails/RogueTrails.java
@@ -1,34 +1,39 @@
+/**
+ * @author Michael Quick
+ * @version v1.0.0
+ * The main class of Rogue Trails. Handles the screens and managers of the game.
+ */
+
 package com.rgd.roguetrails;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class RogueTrails extends ApplicationAdapter {
+/**
+ * {@link com.badlogic.gdx.Game} implementation shared by all platforms.
+ */
+public class RogueTrails extends Game {
     private SpriteBatch batch;
-    private Texture image;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+    }
+
+    /**
+     * Allows all screens to use the same SpriteBatch
+     * @return the SpriteBatch used in the entire game
+     */
+    public SpriteBatch getBatch() {
+        return batch;
     }
 }
